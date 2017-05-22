1ac1397a7f40c6009aa4b6a1cedaaef506745630
Nikhil <nikhilsaraf89@gmail.com>
2017-05-21 19:05:15 -0700

    Added Persistence to the store using 'redux-persist'

--- App.js
+++ App.js
@@ -3,12 +3,31 @@
  */
 
 import React from 'react';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { AsyncStorage } from 'react-native';
+import { compose, applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import AppMain from './AppMain';
 import reducer from './reducers/Reducer';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import immutableTransform from 'redux-persist-transform-immutable';
 
-const store = createStore(reducer);
+// add `autoRehydrate` as an enhancer to the store (note: `autoRehydrate` is not a middleware)
+const store = createStore(
+  reducer,
+  undefined,
+  compose(applyMiddleware(), autoRehydrate())
+);
+
+// begin periodically persisting the store
+// use immutableTransform so we can save and hydrate Immutable objects
+persistStore(
+  store,
+  {
+    storage: AsyncStorage,
+    transforms: [immutableTransform()]
+  },
+  () => { console.log('data rehydration complete!'); }
+);
 
 export default class App extends React.Component {
 
